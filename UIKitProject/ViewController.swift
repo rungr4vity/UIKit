a596ab213e4169013d03a670fa83bec2b1e1ff95
Francisco Martin <il.capitano.bam@gmail.com>
2024-07-08 11:07:22 -0600

    Initial Commit

---
+++ UIKitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitProject
+//
+//  Created by Frank Martin on 08/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
